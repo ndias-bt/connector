e68279380883e6f233c5d7a945ebc3c233b37a28
Nathan Dias <ndias@goboomtown.com>
2022-02-16 17:08:03 -0800

    Move cloud run connector url search to configuration file

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,6 +2,8 @@
 
 import * as os from 'os';
 
+import { google } from 'googleapis';
+
 export const configuration = () => ({
   name: process.env.NAME,
   displayName: process.env.DISPLAY_NAME,
@@ -11,6 +13,56 @@ export const configuration = () => ({
   ipAddress: process.env.IP_ADDRESS,
   hostname: os.hostname(),
   port: process.env.PORT,
-  url: process.env.BASE_URL || os.hostname(),
+  url: getConnectorUrl(),
   registrationUrl: process.env.REGISTRATION_URL,
 });
+
+function getConnectorUrl() {
+  let base_url = process.env.BASE_URL ?? os.hostname();
+
+  const connectorName = process.env?.K_SERVICE;
+
+  if (connectorName) {
+    getCloudRunConnectorUrl(connectorName).then((url: string) => {
+      if (url) {
+        base_url = url;
+      }
+    });
+  }
+  return base_url;
+}
+
+async function getCloudRunConnectorUrl(connectorName: string) {
+  console.log(`### looking up connector ${connectorName} via cloud run api`);
+
+  const connectorsCloudProjectId = 'connector-registry-poc';
+
+  const run = google.run('v1');
+
+  const auth = new google.auth.GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  });
+
+  const authClient = await auth.getClient();
+  google.options({ auth: authClient });
+
+  const params = {
+    parent: `namespaces/${connectorsCloudProjectId}`,
+    watch: false,
+  };
+
+  const results = await run.namespaces.services.list(params);
+
+  const connectorUrls = [];
+
+  results.data.items.forEach((item) => {
+    console.log(`### found connector ${item.metadata.name}`);
+    connectorUrls.push(item.status.url);
+  });
+
+  if (connectorUrls.length === 1) {
+    return connectorUrls[0];
+  } else {
+    return null;
+  }
+}
--- src/services/registration/registration.service.ts
+++ src/services/registration/registration.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
 import { ConfigService } from '@nestjs/config';
 import { Endpoint } from '../../interfaces/endpoint.interface';
 import { Connector } from '../../interfaces/connector.interface';
-import { google } from 'googleapis';
+// import { google } from 'googleapis';
 
 @Injectable()
 export class RegistrationService implements OnApplicationBootstrap {
@@ -40,12 +40,12 @@ export class RegistrationService implements OnApplicationBootstrap {
 
     const connector: Connector = {
       name: this.config.get<string>('name'),
-      base_url: await this.getBaseUrl(),
+      base_url: this.config.get<string>('url'),
       displayName: this.config.get<string>('displayName'),
       description: this.config.get<string>('description'),
       company: this.config.get<string>('company'),
       endpoints: [infoEndpoint, configEndpoint],
-      version: this.config.get<string>('version')
+      version: this.config.get<string>('version'),
     };
 
     console.log('### connector.base_url', connector.base_url);
@@ -55,71 +55,71 @@ export class RegistrationService implements OnApplicationBootstrap {
       .pipe(map((response) => response.data));
   }
 
-  /**
-   * @return baseUrl - the base url of the connector to be registered
-   * Use the google cloud run provided URL if detectable, otherwise
-
-   */
-  async getBaseUrl() {
-    let cloudRunUrl = null;
-    if (process.env.K_SERVICE) {
-      cloudRunUrl = await this.getCloudRunConnectorUrl(process.env.NAME);
-    }
-
-    return cloudRunUrl || this.config.get<string>('url');
-  }
-
-  /**
-   * Searches cloud run api for a connector
-   * @param connectorName
-   */
-  async getCloudRunConnectorUrl(connectorName: string) {
-    console.log(
-      '### looking up connector url for connectorName',
-      connectorName,
-    );
-
-    // TODO: put this default in a config file, with way to override
-    const connectorFarmProjectId = 'connector-registry-poc';
-
-    const run = google.run('v1');
-
-    const auth = new google.auth.GoogleAuth({
-      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-
-    const authClient = await auth.getClient();
-    google.options({ auth: authClient });
-
-    const params = {
-      parent: `namespaces/${connectorFarmProjectId}`,
-      watch: false,
-    };
-
-    // NOTE: we use services.list instead of services.get here
-    //  b/c the run api is coded to hit the global run.googleapis.com
-    //  endpoint instead of the regional endpoints, and the global
-    //  endpoint is restricted to list calls only
-    //
-    //  This is a security risk, since any connector would be able
-    //  to retrieve other connectors registered in the same namespace.
-    //
-    //  TODO: replace this with a services.get call, or other method
-    //  that fetches url for the specific connector only.
-
-    const results = await run.namespaces.services.list(params);
-
-    const connectorUrls = [];
-
-    results.data.items.forEach((item) => {
-      console.log('### found connector:', item.metadata.name);
-
-      if (item.metadata.name === connectorName) {
-        connectorUrls.push(item.status.url);
-      }
-    });
-
-    console.log('### connectorUrls', connectorUrls);
-    return connectorUrls.pop();
-  }
+  // /**
+  //  * @return baseUrl - the base url of the connector to be registered
+  //  * Use the google cloud run provided URL if detectable, otherwise
+  //
+  //  */
+  // async getBaseUrl() {
+  //   let cloudRunUrl = null;
+  //   if (process.env.K_SERVICE) {
+  //     cloudRunUrl = await this.getCloudRunConnectorUrl(process.env.NAME);
+  //   }
+  //
+  //   return cloudRunUrl || this.config.get<string>('url');
+  // }
+  //
+  // /**
+  //  * Searches cloud run api for a connector
+  //  * @param connectorName
+  //  */
+  // async getCloudRunConnectorUrl(connectorName: string) {
+  //   console.log(
+  //     '### looking up connector url for connectorName',
+  //     connectorName,
+  //   );
+  //
+  //   // TODO: put this default in a config file, with way to override
+  //   const connectorFarmProjectId = 'connector-registry-poc';
+  //
+  //   const run = google.run('v1');
+  //
+  //   const auth = new google.auth.GoogleAuth({
+  //     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  //   });
+  //
+  //   const authClient = await auth.getClient();
+  //   google.options({ auth: authClient });
+  //
+  //   const params = {
+  //     parent: `namespaces/${connectorFarmProjectId}`,
+  //     watch: false,
+  //   };
+  //
+  //   // NOTE: we use services.list instead of services.get here
+  //   //  b/c the run api is coded to hit the global run.googleapis.com
+  //   //  endpoint instead of the regional endpoints, and the global
+  //   //  endpoint is restricted to list calls only
+  //   //
+  //   //  This is a security risk, since any connector would be able
+  //   //  to retrieve other connectors registered in the same namespace.
+  //   //
+  //   //  TODO: replace this with a services.get call, or other method
+  //   //  that fetches url for the specific connector only.
+  //
+  //   const results = await run.namespaces.services.list(params);
+  //
+  //   const connectorUrls = [];
+  //
+  //   results.data.items.forEach((item) => {
+  //     console.log('### found connector:', item.metadata.name);
+  //
+  //     if (item.metadata.name === connectorName) {
+  //       connectorUrls.push(item.status.url);
+  //     }
+  //   });
+  //
+  //   console.log('### connectorUrls', connectorUrls);
+  //   return connectorUrls.pop();
+  // }
 }
